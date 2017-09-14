7d366dd81a3c83d71e2c8affef985f54ad5eb646
Alen Pelin <alenpelin@gmail.com>
2017-09-14 17:26:05 +1000

    test

---
+++ src/Sitecore.Support.123.456/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.123.456")]
+[assembly: AssemblyProduct("Sitecore.Support.123.456")]
+[assembly: ComVisible(false)]
